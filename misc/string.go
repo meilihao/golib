803ff3519a2474a02c363b6623583631d3ef42ec
meilihao <563278383@qq.com>
2024-12-26 10:15:19 +0800

    feat(string): + IsDigit

--- misc/string.go
+++ misc/string.go
@@ -1,9 +1,23 @@
 package misc
 
+import (
+	"unicode"
+)
+
 func Reverse(s string) string {
 	runes := []rune(s)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)
+}
+
+func IsDigit(s string) bool {
+	// 遍历字符串中的每个字符
+	for _, r := range s {
+		if !unicode.IsDigit(r) {
+			return false
+		}
+	}
+	return len(s) > 0 // 返回 true 如果字符串非空且全是数字
 }
